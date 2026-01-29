683165c69a2b1a55ae6835477a963ec508617c5c
alizayazhar <alizayazhar26@gmail.com>
2026-01-29 16:18:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function order() {
-  window.open("https://wa.me/92XXXXXXXXXX", "_blank");
-}function addToCart() {
-  alert("Item added to cart 🛒");
+let cart = [];
+let cartCount = document.getElementById("cartCount");
+let orderLink = document.getElementById("orderLink");
+
+function addToCart(product) {
+  cart.push(product);
+  cartCount.innerText = cart.length;
+
+  let message = "Hello, I want to order:%0A" + cart.join("%0A");
+  orderLink.href = "https://wa.me/923709342592?text=" + message;
 }
